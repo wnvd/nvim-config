ac70b3009d1c55c93a095e6ceba3a95ff05de1bb
Naveed Wani <waninaveed998@gmail.com>
2023-06-13 20:27:38 +0530

    harpoon binds changed again

--- lua/usr/harpoon.lua
+++ lua/usr/harpoon.lua
@@ -7,7 +7,7 @@ vim.keymap.set("n", "<leader>a", mark.add_file)
 -- toggle harpoon explorer
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 -- navigation binds to finds 1-4
-vim.keymap.set("n", "<C-1>",function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-2>",function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-3>",function() ui.nav_file(3) end)
--- vim.keymap.set("n", "<C-[>",function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-j>",function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-k>",function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-l>",function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-;>",function() ui.nav_file(4) end)
